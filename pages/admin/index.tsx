b279780d3c5cfa882be8a68d1956f21caa316d4e
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2022-08-17 00:10:01 -0400

    Add admin page

---
+++ pages/admin/index.tsx
@@ -0,0 +1,89 @@
+import { prisma } from '../../db';
+import type { GetServerSideProps, NextPage } from 'next';
+import Header from '../../components/header';
+import { useSession } from 'next-auth/react';
+import styles from './index.module.css';
+import { nextAuthOptions } from '../api/auth/[...nextauth]';
+import { unstable_getServerSession } from 'next-auth';
+
+interface ServerSideProps {
+  errorCode?: number;
+  users?: {
+    id: number;
+    name: string;
+  }[];
+}
+
+export const getServerSideProps: GetServerSideProps<ServerSideProps> = async (ctx) => {
+  const session = await unstable_getServerSession(
+    ctx.req,
+    ctx.res,
+    nextAuthOptions
+  );
+  if (!session || !session.user || !session.user.email) {
+    return {
+      props: {
+        errorCode: 404,
+      },
+    };
+  }
+
+  const currentUser = await prisma.user.findFirst({
+    where: {
+      email: session.user.email,
+      admin: true,
+    },
+  });
+
+  if (!currentUser) {
+    return {
+      props: {
+        errorCode: 401,
+      },
+    };
+  }
+
+  const users = await prisma.user.findMany({});
+  return {
+    props: {
+      users: users.map(u => ({
+        id: u.id,
+        name: u.name,
+      }))
+    }
+  };
+};
+
+const Admin: NextPage<ServerSideProps> = ({
+  errorCode,
+  users,
+}) => {
+  const { data: session } = useSession();
+  return (
+    <div className={styles.root}>
+      <div className={styles.container}>
+      <Header />
+      <main className={styles.main}>
+        <h1>Admin</h1>
+        {session ? (
+          <a href={`/user/${session?.user?.name}`}>Your Page</a>
+        ) : (
+          <></>
+        )}
+        <h2>Users</h2>
+        <ul>
+          {users?.map((user) => (
+            <li key={user.id}>
+              <a href={`/user/${user.name}`}>
+                {user.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </main>
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,17 @@ import { useSession } from 'next-auth/react';
 import styles from './index.module.css';
 
 interface ServerSideProps {
-  users: {
-    id: number;
-    name: string;
-  }[];
 }
 
 export const getServerSideProps: GetServerSideProps<ServerSideProps> = async () => {
-  const users = await prisma.user.findMany({});
   return {
     props: {
-      users: users.map(u => ({
-        id: u.id,
-        name: u.name,
-      }))
+
     }
   };
 };
 
 const Home: NextPage<ServerSideProps> = ({
-  users,
 }) => {
   const { data: session } = useSession();
   return (
@@ -32,22 +23,12 @@ const Home: NextPage<ServerSideProps> = ({
       <div className={styles.container}>
       <Header />
       <main className={styles.main}>
-        <h1>Users</h1>
-
+        <h1>devu</h1>
         {session ? (
           <a href={`/user/${session?.user?.name}`}>Your Page</a>
         ) : (
           <></>
         )}
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>
-              <a href={`/user/${user.name}`}>
-                {user.name}
-              </a>
-            </li>
-          ))}
-        </ul>
       </main>
       </div>
     </div>
